1d5d066292ab5565a2141b7a38b7d065643f85d5
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2021-12-03 15:50:04 +0100

    add images to Menu-Item component

--- e-shop/src/pages/homepage/index.js
+++ e-shop/src/pages/homepage/index.js
@@ -1,40 +1,9 @@
 import './styles.scss';
-import MenuItem from '../../components/Menu-Item';
+import Directory from '../../components/Directory';
 
 const HomePage = () => (
   <div className="homepage">
-    <div className="directory-menu">
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">HATS</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">JACKETS</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">SCNEAKERS</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">WOMEN</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">MEN</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-    </div>
+    <Directory />
   </div>
 
 
