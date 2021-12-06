0d7bcebbd6089992df6e0e53df5a964458ed4948
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2021-12-06 14:20:22 +0100

    configuration firebase key

---
+++ e-shop/src/firebase/utils/index.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth'
+
+
+const config = {
+  apiKey: "AIzaSyDDNfmXhLijswdwJ0zhPxY358TE-EANDBs",
+  authDomain: "e-shop-project-96c5e.firebaseapp.com",
+  projectId: "e-shop-project-96c5e",
+  storageBucket: "e-shop-project-96c5e.appspot.com",
+  messagingSenderId: "317629545807",
+  appId: "1:317629545807:web:6e551ac566ea3ad152593a"
+};
+
+
+firebase.initializeApp(config);
