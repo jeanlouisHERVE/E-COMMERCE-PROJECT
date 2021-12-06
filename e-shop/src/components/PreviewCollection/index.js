a4fa3af6b089f232ee0c5d054d7fd311c3e42150
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2021-12-06 08:38:34 +0100

    styling PreviewCollection Component

--- e-shop/src/components/PreviewCollection/index.js
+++ e-shop/src/components/PreviewCollection/index.js
@@ -1,17 +1,20 @@
 import './styles.scss'; 
-
+import ItemCollection from '../../components/ItemCollection';
 
 const PreviewCollection = ({title, items} ) => {
 
 return (
   <div className="collection-preview">
     <h1 className="title">{title.toUpperCase()}</h1>
-    <div className="preview"></div>
+    <div className="preview">
     {
-      items.map(item => (
-        <div key={item.id}>{item.name}</div>
+      items
+      .filter((item, idx) => idx < 4)
+      .map(({id, ...otherItemProps}) => (
+        <ItemCollection key={id} {...otherItemProps} />
       ))
     }
+    </div>
   </div>
   )
 }
