b5ac54f409064f337b52c8d1157fd5176a5f08b7
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2021-12-06 08:52:31 +0100

    creation and style of Header Component

--- e-shop/src/components/App/App.js
+++ e-shop/src/components/App/App.js
@@ -4,10 +4,12 @@ import './App.css';
 
 import HomePage from '../../pages/homepage';
 import ShopPage from '../../pages/shop';
+import Header from '../../components/Header';
 
 
 const App = () => (
   <div className="App">
+    <Header/>
     <Routes>
         <Route path="/" element={<HomePage/>}/>
         <Route path="/shop" exact element={<ShopPage/>}/>
