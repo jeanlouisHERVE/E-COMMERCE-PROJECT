9cfd5faf7890e9edfa09da07f7da118e33f5450c
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2021-12-08 14:15:45 +0100

    modification of the header component to link him to the reducer

--- e-shop/src/components/App/App.js
+++ e-shop/src/components/App/App.js
@@ -47,7 +47,7 @@ class App extends Component {
   render() {
     return(
       <div className="App">
-        <Header currentUser={this.state.currentUser}/>
+        <Header/>
         <Routes>
             <Route path="/" element={<HomePage/>}/>
             <Route path="/shop" exact element={<ShopPage/>}/>
