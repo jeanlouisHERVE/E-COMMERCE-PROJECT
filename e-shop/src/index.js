d0bf7bf3736b2b3853562efb63a668a5d2461b20
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2021-12-08 13:45:50 +0100

    installation of redux creation of the storeRedux and userReducer

--- e-shop/src/index.js
+++ e-shop/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import { BrowserRouter as Router } from 'react-router-dom';
-
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 
