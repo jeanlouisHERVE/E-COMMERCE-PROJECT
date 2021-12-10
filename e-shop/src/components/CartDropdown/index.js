b338c4149af49ac855483972bbb0536abe4360bd
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2021-12-10 13:33:21 +0100

    replace withrouter from RRD V5 to useNavigate from RRD V6

--- e-shop/src/components/CartDropdown/index.js
+++ e-shop/src/components/CartDropdown/index.js
@@ -1,27 +1,40 @@
 import './styles.scss';
 import { connect } from 'react-redux';
+import { useNavigate } from "react-router-dom";
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 import { createStructuredSelector } from 'reselect';
+
 // import { withRouter } from 'react-router-dom';
 
 // import components
 import CustomButton from '../CustomButton'; 
 import CartItem from '../CartItem';
 
-const CartDropdown = ({cartItems, history}) => ( 
-  <div className="cart-dropdown">
-    <div className="cart-items">
-    {
-      cartItems.length ? (
-        cartItems.map(cartItem => (<CartItem key={cartItem.id} item={cartItem}/>
-        )))
-      : 
-      (<span className="empty-message">Your cart is empty</span>)
-    }
+const CartDropdown = ({cartItems}) => {
+
+  let navigate = useNavigate();
+
+  function handleClick() {
+    navigate('./checkout');
+  }
+  
+
+  return (
+    <div className="cart-dropdown">
+        <div className="cart-items">
+        {
+          cartItems.length ? (
+            cartItems.map(cartItem => (<CartItem key={cartItem.id} item={cartItem}/>
+            )))
+          : 
+          (<span className="empty-message">Your cart is empty</span>)
+        }
+        </div>
+        <CustomButton onClick={handleClick}>go to checkout</CustomButton>
     </div>
-    <CustomButton onClick={() => history.push('./checkout')}>go to checkout</CustomButton>
-  </div>
-)
+  )
+} 
+
 
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems
