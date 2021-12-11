ad8a2be52999b597bbc4120acde0b0ed089612a5
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2021-12-11 15:24:23 +0100

    add directory state to redux

--- e-shop/src/components/Directory/index.js
+++ e-shop/src/components/Directory/index.js
@@ -1,62 +1,23 @@
 
 import './styles.scss';
-import { Component } from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selecDrectorySections } from '../../redux/directory/directory.selectors';
 import MenuItem from '../../components/Menu-Item';
 
-class Directory extends Component {
-  constructor() {
-    super()
-
-    this.state = {
-
-    sections: [
-        {
-          title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          id: 1,
-          linkUrl: 'shop/hats'
-        },
-        {
-          title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          id: 2,
-          linkUrl: 'shop/jackets'
-        },
-        {
-          title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          id: 3,
-          linkUrl: 'shop/sneakers'
-        },
-        {
-          title: 'womens',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-          size: 'large',
-          id: 4,
-          linkUrl: 'shop/womens'
-        },
-        {
-          title: 'mens',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-          size: 'large',
-          id: 5,
-          linkUrl: 'shop/mens'
-        }
-      ]
+const Directory = ({sections}) =>(
+  <div className="directory-menu">
+    {
+      sections.map(({ id, ...otherSectionProps }) => (
+        <MenuItem key={id} {...otherSectionProps}/>
+      ))
     }
+  </div>
+)
 
-  }
-  render () {
-    return (
-      <div className="directory-menu">
-        {
-          this.state.sections.map(({title, imageUrl, id, size}) => (
-            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
-          ))
-        }
-      </div>
-    )
-  }
-}
+const mapStateToProps = createStructuredSelector({
+  sections : selecDrectorySections
+})
+  
 
-export default Directory
+export default connect(mapStateToProps)(Directory)
